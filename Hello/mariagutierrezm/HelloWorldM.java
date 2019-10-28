e24931a34b37ee5afcc964ab97aa116ae4ec321c
Maria <maria.gutierrez>
2019-10-28 17:29:06 +0000

    first ever push hello world

---
+++ Hello/mariagutierrezm/HelloWorldM.java
@@ -0,0 +1,13 @@
+
+public class HelloWorldM {
+
+	public static String hello() {
+	    return "Hello World";
+    }
+	
+	public static void main(String[] args) {
+		
+		 System.out.println(hello()); //prints out Hello World in console
+	}
+
+}
