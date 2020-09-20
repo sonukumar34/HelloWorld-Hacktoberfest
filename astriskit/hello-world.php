6bc559255534f51a91af86553861619b788776cc
H S <harry2office@gmail.com>
2020-09-20 19:51:16 +0530

    Added hello-world in php

---
+++ astriskit/hello-world.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* To run using php-interpreter on local machine, 
+* > Make sure php is installed
+* > download this file and type "php ./hello-world.php" on the console; Don't forget to switch directory to downloaded one for this one.
+*/
+function main(){
+  echo("Hello World"); // prints "Hello World"
+}
+
+main();
+?>
