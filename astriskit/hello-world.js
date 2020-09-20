21d292b86874b5076e515d1233037f79c6558101
H S <harry2office@gmail.com>
2020-09-20 19:03:11 +0530

    added instructions to run the code

--- astriskit/hello-world.js
+++ astriskit/hello-world.js
@@ -1,3 +1,11 @@
+/**
+* Instruction to run this file using node: 
+* > install node-js
+* > copy content of this file and save it or download it
+* > run the file using: node hello-world.js
+* > next "Hello World" should be seen on the console, if all went well. :)
+*/
+
 const main = () => {
   console.log("Hello World"); // prints "Hello World" to the console
 }
